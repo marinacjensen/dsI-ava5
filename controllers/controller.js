7cdb23a8534e901becace76d84f09d72c130a38c
marinacjensenn <84468498+marinacjensenn@users.noreply.github.com>
2023-09-15 08:48:43 -0300

    Criacão do controller e rotas

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,12 @@
-const { sequelize, Sequelize } = require('../config/database');
+const { sequelize, Sequelize } = require ('../config/database');
+
+const livros = require('../models/livros')(sequelize, Sequelize)
+const usuarios = require('../models/usuarios')(sequelize,Sequelize)
 
 exports.index = (req, res) => {
         res.render('index');
     };
+
+exports.registerForm = (req, res) => {
+    res.render('registerForm');
+};
--- routes/approutes.js
+++ routes/approutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 var controller = require("../controllers/controller");
 
+
 router.get("/", controller.index);
+router.get("/registro", controller.registerForm);
 
 module.exports = router;
