4a503ffcf0d8501c64e8077e11ba442c2cfd9009
marinacjensenn <84468498+marinacjensenn@users.noreply.github.com>
2023-09-20 10:53:03 -0300

    Criação dos controllers e rotas

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,12 +1,110 @@
-const { sequelize, Sequelize } = require ('../config/database');
+const {
+    sequelize,
+    Sequelize
+} = require('../config/database');
+const bcrypt = require('bcrypt')
 
 const livros = require('../models/livros')(sequelize, Sequelize)
-const usuarios = require('../models/usuarios')(sequelize,Sequelize)
+const usuarios = require('../models/usuarios')(sequelize, Sequelize)
 
 exports.index = (req, res) => {
-        res.render('index');
-    };
+    res.render('index');
+};
 
 exports.registerForm = (req, res) => {
     res.render('registerForm');
+};
+
+exports.addUser = async (req, res) => {
+    const {
+        nome,
+        email,
+        senha
+    } = req.body;
+    const senha_hashed = await bcrypt.hash(senha, 10)
+    await usuarios.create({
+        nome,
+        email,
+        senha_hashed
+    });
+    res.redirect('/');
+}
+
+exports.showUser = (req, res) => {
+    res.render('user');
+}
+
+exports.showBooks = async (req, res) => {
+    const results = await livros.findAll({
+        order: [
+            ['id', 'ASC']
+        ]
+    });
+    res.render('books', {
+        results
+    });
+};
+
+exports.registerBooks = (req, res) => {
+    res.render('registerBooks');
+};
+
+exports.addBooks = async (req, res) => {
+    const {
+        autor,
+        titulo,
+        ano,
+        quantidade
+    } = req.body;
+    const user = null;
+    await livros.create({
+        autor,
+        titulo,
+        ano,
+        quantidade,
+        user
+    });
+    res.redirect('/livros');
+}
+
+exports.deleta = async (req, res) => {
+    const id = req.params.id;
+    await livros.destroy({
+        where: {
+            id
+        }
+    });
+    res.redirect('/livros');
+};
+
+exports.edita = async (req, res) => {
+    const id = req.params.id;
+    const livro = await livros.findByPk(id);
+    res.render('editBooks', {
+        id,
+        livro
+    });
+};
+
+exports.atualiza = async (req, res) => {
+    const id = req.params.id;
+    const {
+        autor,
+        titulo,
+        ano,
+        quantidade
+    } = req.body;
+    const user = null;
+    await empregados.update({
+        autor,
+        titulo,
+        ano,
+        quantidade,
+        user
+    }, {
+        where: {
+            id
+        }
+    });
+    res.redirect('/livros');
 };
--- routes/approutes.js
+++ routes/approutes.js
@@ -5,5 +5,13 @@ var controller = require("../controllers/controller");
 
 router.get("/", controller.index);
 router.get("/registro", controller.registerForm);
+router.post("/registro", controller.addUser);
+router.get("/usuario", controller.showUser);
+router.get("/livros", controller.showBooks);
+router.get("/registroLivros", controller.registerBooks);
+router.post("/registroLivros", controller.addBooks);
+router.get("/deleta/:id", controller.deleta);
+router.get("/edita/:id", controller.edita);
+router.post("/edita/:id", controller.atualiza)
 
 module.exports = router;
