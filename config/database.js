fdadb476834c9fe87a3d4e78f45a81c9cdbf80c5
marinacjensenn <marina.jensen@aluno.riogrande.ifrs.edu.com>
2023-10-02 08:26:03 -0300

    .

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize({
   database: 'marina',
   username: 'root',
-  password: '',
+  password: 'mysqluser',
   host: 'localhost',
   dialect: 'mysql'
 });
--- index.js
+++ index.js
@@ -1,31 +1,21 @@
 const express = require('express');
-const {
-    engine
-} = require('express-handlebars');
-const handlebars = require('handlebars');
-const {
-    allowInsecurePrototypeAccess
-} = require('@handlebars/allow-prototype-access');
+const path = require('path');
 const bodyParser = require('body-parser');
+const app = express();
+const handlebars = require('express-handlebars');
+const handlebars_mod = require('handlebars');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
+const appRoutes = require('./routes/approutes');
 const session = require('express-session');
 const flash = require('connect-flash');
-const path = require('path');
-const router = require('./routes/router');
-const sequelize = require('./config/database');
 
 sequelize.sync();
 
-const app = express();
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-
-app.use(session({
-    secret: 'secretkey',
-    resave: false,
-    saveUninitialized: false
-}));
+app.use(express.static('public'))
 
 app.use(flash());
 app.use((req, res, next) => {
@@ -35,17 +25,38 @@ app.use((req, res, next) => {
     next();
 });
 
-app.engine('.hbs', engine({
-    extname: '.hbs',
-    handlebars: allowInsecurePrototypeAccess(handlebars)
+// Handlebars configuration
+const helpers = require('./helpers/helpers'); // Import the helpers
+const { sequelize } = require('./config/database');
+app.engine('handlebars', handlebars.engine({
+    defaultLayout: false,
+    handlebars: allowInsecurePrototypeAccess(handlebars_mod),
+    extname: '.handlebars',
+    helpers: helpers
 }));
-app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'secretkey',
+    resave: false,
+    saveUninitialized: false
+}));
 
-app.use(router);
+// Sync the models with the database
+sequelize.sync()
+    .then(() => {
+        console.log('Database synced sucessfully');
+    })
+    .catch((error) => {
+        console.error('Error syncing database:', error);
+    });
 
-app.listen(3000, () => {
-    console.log(`App listening on port 3000`);
+// Routes
+app.use(appRoutes);
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
 });
